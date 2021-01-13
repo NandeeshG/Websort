7ad34095b914d9558bec48edebbb59576ee3c150
NandeeshG <nandeeshgupta@gmail.com>
2021-01-13 21:47:21 +0530

    removed docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
   router: {
-    base: "/Websort/docs/"
+    base: "/Websort/"
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
